b4d25d7391624b212220a78c0020bfc68adb2dd2
Eleni Pastra <hell-en@Elenis-MacBook-Pro.local>
2018-07-09 22:34:25 -0700

    html and a tiny amount of js

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+// Starting array of animals
+var animals =["Cats", "Dogs", "Baby Goats", "Piglets", "Parrots" , "Baby Polar Bears"];
+
+ // Event listener for all button elements
+ $("button").on("click", function() {
+     console 
+    // In this case, the "this" keyword refers to the button that was clicked
+    var animals = $(this).attr("data-animal");
+
+ });
